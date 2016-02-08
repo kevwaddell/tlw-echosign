07b884b162a4d47ceb823160147373a05c7c4223
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-02-08 10:30:00 +0000

    Remove file extension function.

--- tests/checkmail.php
+++ tests/checkmail.php
@@ -5,11 +5,6 @@ include_once($_SERVER[DOCUMENT_ROOT].'/inc/pre-function.php');
 include_once($_SERVER[DOCUMENT_ROOT].'/inc/current_pg_function.php');
 include_once($_SERVER[DOCUMENT_ROOT].'/classes/PHPMailer/PHPMailerAutoload.php');
 
-function getFileExtension($fileName){
-   $parts=explode(".",$fileName);
-   return $parts[count($parts)-1];
-}
-
  
 // Connect to gmail
 $imapPath = "{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX";
