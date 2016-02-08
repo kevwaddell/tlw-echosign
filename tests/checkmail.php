ecc3849e9c61a4216f4983b7dd44b56a1f628e5a
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-02-08 10:40:43 +0000

    revert to TLW email account and add current page function as include.

--- tests/checkmail.php
+++ tests/checkmail.php
@@ -1,19 +1,18 @@
 <?php
 //set_time_limit(4000);
-
 include_once($_SERVER[DOCUMENT_ROOT].'/inc/pre-function.php');
 include_once($_SERVER[DOCUMENT_ROOT].'/inc/current_pg_function.php');
 include_once($_SERVER[DOCUMENT_ROOT].'/classes/PHPMailer/PHPMailerAutoload.php');
-
  
 // Connect to gmail
+/*
 $imapPath = "{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX";
 $username = "kwaddelltlw@gmail.com";
 $password = "TLW_kevin21";
+*/
 
 
 // Connect to TLW imap
-/*
 if ($host == "tlw-echosign.dev") {
 	$imapPath = "{192.168.12.9:143/imap/notls/novalidate-cert/user=esign}INBOX";	
 } else {
@@ -21,7 +20,6 @@ if ($host == "tlw-echosign.dev") {
 }
 $username = "esign@tlwsolicitors.co.uk";
 $password = "document5";
-*/
 
 $inbox = imap_open($imapPath, $username, $password, NULL, 1, array('DISABLE_AUTHENTICATOR' => 'GSSAPI'));
 
