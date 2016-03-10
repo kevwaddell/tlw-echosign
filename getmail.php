0f91c9b433aaa3c63688c75cabe8453955bbb10d
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-10 16:38:03 +0000

    Remove set timelimit.

--- getmail.php
+++ getmail.php
@@ -1,5 +1,4 @@
 <?php
-set_time_limit(4000);
 include_once($_SERVER['DOCUMENT_ROOT'].'/inc/current_pg_function.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/inc/pre-function.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/classes/PHPMailer/PHPMailerAutoload.php');
