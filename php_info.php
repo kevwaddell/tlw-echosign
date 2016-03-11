48dfa3ac223c3d8f048964dace552e232a505ee3
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-11 11:59:53 +0000

    Add phpinfo() file.

---
+++ php_info.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<title>PHP Information</title>
+<link rel="stylesheet" href="<?php echo SITEROOT; ?>/assets/css/global-css.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+<body>
+	<div class="container">
+	<?php phpinfo(); ?>	
+	</div>
+</body>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</html>
