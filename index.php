4da5c88976591adc54eccbbe99c7a149dcef2e2b
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-11 14:43:58 +0000

    Home page fix unsigned variable.

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $unsigned_data = unserialize($raw_unsigned_data);
 	}
 	
 	if (!$unsigned && !$noref ) {
-	$raw_signed_data = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/logs/sent_data.txt');
+	$raw_signed_data = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/logs/sent_data-'.$log_date.'.log');
 	$signed_data = unserialize($raw_signed_data);	
 	//pre($signed_data);
 		foreach ($signed_data as $k => $sd) {
