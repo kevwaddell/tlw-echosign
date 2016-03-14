7a108683bac8dd7b10585277e44ff039125c8522
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-14 16:12:49 +0000

    Start admin.

---
+++ admin/dashboard.php
@@ -0,0 +1,47 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/inc/pre-function.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/inc/current_pg_function.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<meta name="robots" content="noindex,nofollow">
+<title>TLW Solicitors | Email logs</title>
+<link rel="stylesheet" href="<?php echo SITEROOT; ?>/assets/css/global-css.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<?php
+	
+?>
+</head>
+<body>
+	
+	<?php include_once($_SERVER['DOCUMENT_ROOT'].'/inc/html/col-strip.php'); ?>
+	
+	<?php include_once($_SERVER['DOCUMENT_ROOT'].'admin/inc/html/admin-header.inc'); ?>
+	
+	<main class="main-content">
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+										
+				</div>
+			</div>
+		</div>
+			
+	</main>
+
+	<footer class="app-info">
+		<div class="container">
+			<small>&copy; TLW Solicitors 2016. All rights reserved.</small>
+		</div>
+	</footer>
+
+</body>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="<?php echo SITEROOT; ?>/assets/js/app.js"></script>
+</html>
