b634e51be3396cdd0682e9025867af5847ae9e45
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-15 12:51:36 +0000

    Add renew email link messages on letter template.

---
+++ inc/html/letter-renew-sent-error-message.php
@@ -0,0 +1,10 @@
+<div class="letter-outer">
+	<div class="letter-logo-wide"></div>
+	<div class="well well-lg text-center no-doc-well bg-ltgray">
+		<i class="glyphicon glyphicon-file"></i>
+		<h1 class="txt-tlw-red caps">Error sending new link</h1>
+		<p class="lead">Sorry there was an error trying to send the new document link.</p>
+		<p class="bold">Please contact the technical support team to request a new link.</p>					
+		<a href="mailto:webmaster@tlwsolicitors.co.uk" class="btn btn-default btn-block btn-lg"><i class="glyphicon glyphicon-comment"></i> Contact support</a>
+	</div>
+</div>
---
+++ inc/html/letter-renew-sent-message.php
@@ -0,0 +1,10 @@
+<div class="letter-outer">
+	<div class="letter-logo-wide"></div>
+	<div class="well well-lg text-center no-doc-well bg-ltgray">
+		<i class="glyphicon glyphicon-file"></i>
+		<h1 class="txt-tlw-red caps">Document link sent</h1>
+		<p class="lead">A new document link has been sent to the following email address.</p>
+		<p class="bold"><?php echo $data['email']; ?></p>					
+		<a href="<?php echo SITEROOT; ?>" class="btn btn-default btn-block btn-lg"><i class="glyphicon glyphicon-refresh"></i> Continue</a>
+	</div>
+</div>
