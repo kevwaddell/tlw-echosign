06790f0d31807f189687db2ef9cfe29971caa42d
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-23 11:57:22 +0000

    Initiate log files cron function.

---
+++ admin/logs/archives/logs-cron.php
@@ -0,0 +1,8 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/inc/pre-function.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/inc/current_pg_function.php');
+
+$log_date = date('Y-m-d', time());
+
+pre($log_date);
+?>
