8116c61f79d5c772bf8e9d03459cf7a107094eea
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-23 10:10:56 +0000

    Change get mail function to send all unread client emails.

--- inc/emails/send-client-email.php
+++ inc/emails/send-client-email.php
@@ -4,19 +4,19 @@ function sendClientEmail() {
 								
 	$body = file_get_contents(SITEROOT.'/temps/client-email-notify.php?cref='.$data['ref']);
 	
-	$mail = new PHPMailer();
+	$mail_$data['ref'] = new PHPMailer();
 	
 	//SMTP settings
-	$mail->isSMTP();
+	$mail_$data['ref']->isSMTP();
 	include_once($_SERVER['DOCUMENT_ROOT'].'/inc/tlw-smtp.php');		
 	
 	//Sending options	
-	$mail->AddReplyTo(TLW_REPLY_EMAIL, TLW_REPLY_NAME);
-	$mail->SetFrom(TLW_SOURCE_EMAIL, TLW_SOURCE_NAME);
-	$mail->AddAddress($data['email']);
-	$mail->Subject = "Your TLW Solicitors Client Agreement is ready to sign";
-	$mail->MsgHTML($body);
+	$mail_$data['ref']->AddReplyTo(TLW_REPLY_EMAIL, TLW_REPLY_NAME);
+	$mail_$data['ref']->SetFrom(TLW_SOURCE_EMAIL, TLW_SOURCE_NAME);
+	$mail_$data['ref']->AddAddress($data['email']);
+	$mail_$data['ref']->Subject = "Your TLW Solicitors Client Agreement is ready to sign";
+	$mail_$data['ref']->MsgHTML($body);
 	
-	return $mail->Send();
+	return $mail_$data['ref']->Send();
 }	
 ?>
