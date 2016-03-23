0c789c61cb6099336c15a4302e344aafc54ef524
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-23 10:21:23 +0000

    Re test send client email function.

--- inc/emails/send-client-email.php
+++ inc/emails/send-client-email.php
@@ -4,18 +4,20 @@ function sendClientEmail() {
 								
 	$body = file_get_contents(SITEROOT.'/temps/client-email-notify.php?cref='.$data['ref']);
 	
-	$mail_.$data['ref']) = new PHPMailer();
+	$mail_.$data['ref'] = new PHPMailer();
 	
 	//SMTP settings
-	$mail_.$data['ref'])->isSMTP();
+	$mail_.$data['ref']->isSMTP();
 	include_once($_SERVER['DOCUMENT_ROOT'].'/inc/tlw-smtp.php');		
 	
 	//Sending options	
-	$mail_.$data['ref'])->AddReplyTo(TLW_REPLY_EMAIL, TLW_REPLY_NAME);
-	$mail_.$data['ref'])->SetFrom(TLW_SOURCE_EMAIL, TLW_SOURCE_NAME);
-	$mail_.$data['ref'])->AddAddress($data['email']);
-	$mail_.$data['ref'])->Subject = "Your TLW Solicitors Client Agreement is ready to sign";
-	$mail_.$data['ref'])->MsgHTML($body);
+	$mail_.$data['ref']->AddReplyTo(TLW_REPLY_EMAIL, TLW_REPLY_NAME);
+	$mail_.$data['ref']->SetFrom(TLW_SOURCE_EMAIL, TLW_SOURCE_NAME);
+	$mail_.$data['ref']->AddAddress($data['email']);
+	$mail_.$data['ref']->Subject = "Your TLW Solicitors Client Agreement is ready to sign";
+	$mail_.$data['ref']->MsgHTML($body);
+	
+	pre($mail_.$data['ref']);
 	
 	return $mail_.$data['ref'])->Send();
 }	
