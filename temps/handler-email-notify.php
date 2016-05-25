e06fb06d1682853203b978c419932a590a1355a4
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-05-25 15:07:15 +0100

    Update handler email notification.

--- temps/handler-email-notify.php
+++ temps/handler-email-notify.php
@@ -35,12 +35,12 @@ $cref = $data['ref'];
 		</div>
 		
 		<div class="content" style="padding: 20px 40px; font-size: 16px; line-height: 20px; min-height: 100px;">
-			<h2 align="center" style="color: #ca156e;">TLW Solicitors Client Agreement has not been signed yet</h2>
-			<p align="center">Client name: <?php echo $name; ?>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Client ref: <?php echo $cref; ?></p>
-			<p align="center">Please go to the following link to resend the notification email.</p>
-			<p align="center"><a href="<?php echo SITEROOT; ?>/sendmail/?cref=<?php echo $cref; ?>" style="display: inline-block; padding: 10px; background-color: #ACD15C; text-transform: uppercase; color: white; font-weight: bold;">Send Now</a></p>
+			<h2 align="center" style="color: #ca156e;">TLW Solicitors Client Agreement has not been signed</h2>
+			<p align="center bold">Client name: <?php echo $name; ?>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Client ref: <?php echo $cref; ?></p>
+			<p align="center">Please click on the link below to resend the notification email.</p>
+			<p align="center"><a href="<?php echo SITEROOT; ?>/sendmail/?cref=<?php echo $cref; ?>" style="display: inline-block; padding: 10px; background-color: #ACD15C; text-transform: uppercase; color: white; font-weight: bold; text-decoration: none;">Re-send notification email</a></p>
 			<p align="center">If you have any problems viewing the link above.</p>
-			<p align="center"><strong>Please notify the website administrator <a href="mailto: webmaster@tlwsolicitors.co.uk" style="color: #ca156e;">webmaster@tlwsolicitors.co.uk</a> or call EXT: 210</strong></p>
+			<p align="center"><strong>Please notify the website administratoron: <br /><a href="mailto: webmaster@tlwsolicitors.co.uk" style="color: #ca156e;">webmaster@tlwsolicitors.co.uk</a> or call EXT: 210</strong></p>
 		</div>
 		
 		<div class="footer" style="background-color: #666; padding: 20px; color: white; font-size: 12px; text-align: center;">
