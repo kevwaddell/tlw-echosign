4b559d1fec1702f5890cf95e561c31d098bbce66
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-05-26 14:50:49 +0100

    Update referer script.

--- signed/rmvdata.php
+++ signed/rmvdata.php
@@ -2,9 +2,11 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/inc/pre-function.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/inc/current_pg_function.php');
 
+if (in_array('HTTP_REFERER', $_SERVER)) {
 $referer_raw = $_SERVER['HTTP_REFERER'];
 $referer_parse = parse_url($referer_raw);
-$referer = $referer_parse['scheme']."://".$referer_parse['host'].$referer_parse['path'];
+$referer = $referer_parse['scheme']."://".$referer_parse['host'].$referer_parse['path'];	
+}
 $now = time();
 	
 if (isset($_GET['cref']) && isset($_GET['log'])) {
