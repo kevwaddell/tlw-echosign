988301b8e6d0ba2eb70cc8b3340bc9f1d27fef8c
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-08-25 15:50:29 +0100

    Fixes to remove zip script.

--- signed/rmvzip.php
+++ signed/rmvzip.php
@@ -1,6 +1,7 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/inc/pre-function.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/inc/current_pg_function.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/inc/global-settings.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/classes/PHPMailer/PHPMailerAutoload.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/inc/emails/send-IT-zip-email.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/inc/emails/send-IT-all-zips-email.php');
