5866ce1245d5d117863e037eb92e6d2758c1ff0b
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-08-26 10:59:36 +0100

    Add test check mail.

--- tests/checkmail.php
+++ tests/checkmail.php
@@ -1,25 +1,14 @@
 <?php
 //set_time_limit(4000);
-include_once($_SERVER['DOCUMENT_ROOT'].'/inc/pre-function.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/inc/current_pg_function.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/inc/global-settings.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/inc/pre-function.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/classes/PHPMailer/PHPMailerAutoload.php');
- 
-// Connect to gmail
-/*
-$imapPath = "{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX";
-$username = "kwaddelltlw@gmail.com";
-$password = "TLW_kevin21";
-*/
-
 
 // Connect to TLW imap
-if ($host == "tlw-echosign.dev") {
-	$imapPath = "{192.168.12.9:143/imap/notls/novalidate-cert/user=esign}INBOX";	
-} else {
-	$imapPath = "{nsgateway.tlwsolicitors.co.uk:143/imap/notls/novalidate-cert/user=esign}INBOX";	
-}
-$username = "esign@tlwsolicitors.co.uk";
-$password = "document5";
+$imapPath = "{".TLW_SMTP_HOST.":143/imap/notls/novalidate-cert/user=esign}INBOX";
+$username = TLW_SOURCE_EMAIL;
+$password = TLW_SMTP_PWD;
 
 $inbox = imap_open($imapPath, $username, $password, NULL, 1, array('DISABLE_AUTHENTICATOR' => 'GSSAPI'));
 
